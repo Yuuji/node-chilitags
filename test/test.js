c4d728e70a942edd52c44459062c8bfd31127ff4
Yuuji <desu@yuuji.de>
2014-08-24 00:36:41 +0200

    - extend test

--- test/test.js
+++ test/test.js
@@ -1,15 +1,17 @@
 var Chilitags = require('../build/Release/chilitags');
+var fs = require('fs');
 
 var chilitags = new Chilitags.Chilitags();
 
+var filename = process.cwd() + '/image.png';
+
 function detect() {
-	var tags = chilitags.detect();
+	var tags = chilitags.detect(filename);
 	for (var key in tags.tags) {
 		var tag = tags.tags[key];
 		console.log(tag.id + ': ' + tag.center.x + ' - ' + tag.center.y);
 	}
-
-	setTimeout(detect, 1);
+	setTimeout(detect, 100);
 }
 
 detect();
